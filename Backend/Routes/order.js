3bef0cc5c9f597eee1eb3a88e306999a03b7eb69
Dharani Dharan <141510215+DharaniDharanThiyagu@users.noreply.github.com>
2024-08-08 11:47:52 +0530

    Delete Backend directory

--- Backend/Routes/order.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { createOrder } = require('../Controllers/OrderController');
-const router = express.Router();
-
-// Define routes with correct method chaining
-router.route('/orders').post(createOrder);
-
-module.exports = router;
